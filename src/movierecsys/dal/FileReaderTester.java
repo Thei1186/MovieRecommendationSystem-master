94dfd13b14df28d388a5f67fdfdb3c1b4664641c
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2018-11-12 12:29:33 +0100

    Ændrede fileReaderTester
    Udkommenterede user testen for at lettere kunne teste getAllMovies metoden

--- src/movierecsys/dal/FileReaderTester.java
+++ src/movierecsys/dal/FileReaderTester.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Stream;
+import movierecsys.be.Movie;
 
 /**
  *
@@ -31,31 +32,41 @@ public class FileReaderTester
      */
     public static void main(String[] args) throws IOException
     {
-        File ratings = new File("data/ratings.txt");   
-        Set<Integer> uniqueIds = new HashSet<>();
-        Stream<String> lines = Files.lines(ratings.toPath());
-        lines.forEach((String t) ->
+        MovieDAO movieDao = new MovieDAO();
+        List<Movie> allMovies = movieDao.getAllMovies();
+        for (Movie movie : allMovies)
         {
-            String[] rating = t.split(",");
-            int id = Integer.parseInt(rating[1]);
-            uniqueIds.add(id);
-        });
-        TreeSet<Integer> sorted = new TreeSet<>(uniqueIds);
-        
-        File users = new File("data/users.txt");
-        try (BufferedWriter bw = Files.newBufferedWriter(users.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE))
-        {
-            File source = new File("C:\\Users\\pgn\\Desktop\\Netflix Data\\load_employees.txt");
-            List<String> emplyees = Files.readAllLines(source.toPath());
-            Iterator<Integer> it = sorted.iterator();
-            for (int i = 0; it.hasNext() && i < emplyees.size(); i++)
-            {
-                String[] cols = emplyees.get(i).split(",");
-                String firstName = cols[2].replace("\'", "");
-                String lastName = cols[3].replace("\'", "");
-                bw.write(Integer.toString(it.next()) + "," + firstName + " " + lastName);
-                bw.newLine();
-            }
+            System.out.println(movie.getTitle());
         }
+        System.out.println("Movie count: " + allMovies.size());
     }
+        
+//        
+//        File ratings = new File("data/ratings.txt");   
+//        Set<Integer> uniqueIds = new HashSet<>();
+//        Stream<String> lines = Files.lines(ratings.toPath());
+//        lines.forEach((String t) ->
+//        {
+//            String[] rating = t.split(",");
+//            int id = Integer.parseInt(rating[1]);
+//            uniqueIds.add(id);
+//        });
+//        TreeSet<Integer> sorted = new TreeSet<>(uniqueIds);
+//        
+//        File users = new File("data/users.txt");
+//        try (BufferedWriter bw = Files.newBufferedWriter(users.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE))
+//        {
+//            File source = new File("C:\\Users\\pgn\\Desktop\\Netflix Data\\load_employees.txt");
+//            List<String> emplyees = Files.readAllLines(source.toPath());
+//            Iterator<Integer> it = sorted.iterator();
+//            for (int i = 0; it.hasNext() && i < emplyees.size(); i++)
+//            {
+//                String[] cols = emplyees.get(i).split(",");
+//                String firstName = cols[2].replace("\'", "");
+//                String lastName = cols[3].replace("\'", "");
+//                bw.write(Integer.toString(it.next()) + "," + firstName + " " + lastName);
+//                bw.newLine();
+//            }
+//        }
+//    }
 }
