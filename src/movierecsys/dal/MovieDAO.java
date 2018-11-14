3a111606c7753e1e26c1d871100084f81d397594
rich0285 <44639744+rich0285@users.noreply.github.com>
2018-11-14 09:39:29 +0100

    ligegyldig fil

--- src/movierecsys/dal/MovieDAO.java
+++ src/movierecsys/dal/MovieDAO.java
@@ -25,19 +25,17 @@ import movierecsys.be.Movie;
 public class MovieDAO
 {
 
-    private static final String MOVIE_SOURCE = "data/movie_titles.txt";
+    private static final String SOURCE = "data/movie_titles.txt";
 
     /**
      * Gets a list of all movies in the persistence storage.
      *
      * @return List of movies.
      */
-    public List<Movie> getAllMovies() throws FileNotFoundException, IOException
+    public List<Movie> getAllMovies() throws IOException
     {
         List<Movie> allMovies = new ArrayList<>();
-
-        File file = new File(MOVIE_SOURCE);
-
+        File file = new File(SOURCE);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file)))
         {
@@ -60,15 +58,6 @@ public class MovieDAO
         }
         return allMovies;
     }
-
-    /**
-     * Reads a movie from a , s
-     *
-     * @param t
-     * @return
-     * @throws NumberFormatException
-     */
-
     private Movie stringArrayToMovie(String t)
     {
         String[] arrMovie = t.split(",");
@@ -91,7 +80,7 @@ public class MovieDAO
      */
     public Movie createMovie(int releaseYear, String title) throws IOException
     {
-        Path path = new File(MOVIE_SOURCE).toPath();
+        Path path = new File(SOURCE).toPath();
         int id = -1;
         try(BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.SYNC, StandardOpenOption.APPEND, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.READ))
         {
@@ -149,7 +138,7 @@ public class MovieDAO
      */
     public Movie getMovie(int id) throws FileNotFoundException, IOException
     {
-        File file = new File(MOVIE_SOURCE);
+        File file = new File(SOURCE);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(file)))
         {
